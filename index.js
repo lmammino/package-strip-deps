3e0c5b1233297f7dee4967c44c7c934da4378f6b
Luciano Mammino <luciano@planet9energy.com>
2017-10-04 14:38:59 +0100

    implement keep dependencies feature

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
-var stdin = process.stdin;
-var stdout = process.stdout;
-var content = '';
+var includes = require('array-includes')
+var yargs = require('yargs')
+var stdin = process.stdin
+var stdout = process.stdout
+var content = ''
 
-stdin.resume();
-stdin.setEncoding('utf8');
+var argv = yargs
+  .option('keep', {
+    describe: 'the name of one or more packages to keep',
+    default: [],
+    type: 'array'
+  })
+  .argv
+
+var removeDeps = function (data, key, keep) {
+  if (data[key]) {
+    for (var property in data[key]) {
+      if (!includes(keep, property)) {
+        delete data[key][property]
+      }
+    }
+  }
+}
+
+stdin.resume()
+stdin.setEncoding('utf8')
 
 stdin.on('data', function (chunk) {
-  content += chunk;
-});
+  content += chunk
+})
 
 stdin.on('end', function () {
-  var parsedData = JSON.parse(content);
-  delete parsedData.dependencies;
-  delete parsedData.devDependencies;
-  var outputJSON = JSON.stringify(parsedData, null, 2);
-  stdout.write(outputJSON);
-  stdout.write('\n');
-});
+  var parsedData = JSON.parse(content)
+  removeDeps(parsedData, 'dependencies', argv.keep)
+  removeDeps(parsedData, 'devDependencies', argv.keep)
+  var outputJSON = JSON.stringify(parsedData, null, 2)
+  stdout.write(outputJSON)
+  stdout.write('\n')
+})
--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-'use strict';
+'use strict'
 
-var path = require('path');
-var test = require('tap').test;
+var childProcess = require('child_process')
+var path = require('path')
+var test = require('tap').test
 
 test('it should strip "dependencies" and "devDependencies" from an input JSON', function (t) {
   var input = JSON.stringify({
@@ -16,29 +17,73 @@ test('it should strip "dependencies" and "devDependencies" from an input JSON', 
       somedep3: '3.4.5',
       somedep4: '6.7.8'
     }
-  });
+  })
 
   var expectedOutput = {
     foo: 'bar',
     baz: 'bat',
-    bat: 'man'
-  };
+    bat: 'man',
+    dependencies: {},
+    devDependencies: {}
+  }
 
-  var executable = path.join(__dirname, 'index.js');
-  var terminal = require('child_process').spawn(executable);
+  var executable = path.join(__dirname, 'index.js')
+  var terminal = childProcess.spawn(executable)
 
-  var output = '';
+  var output = ''
   terminal.stdout.on('data', function (data) {
-    output += data;
-  });
+    output += data
+  })
 
   terminal.on('exit', function (code) {
-    var parsedOutput = JSON.parse(output);
-    t.equals(code, 0);
-    t.same(parsedOutput, expectedOutput);
-    t.end();
-  });
+    var parsedOutput = JSON.parse(output)
+    t.equals(code, 0)
+    t.same(parsedOutput, expectedOutput)
+    t.end()
+  })
 
-  terminal.stdin.write(input);
-  terminal.stdin.end();
-});
+  terminal.stdin.write(input)
+  terminal.stdin.end()
+})
+
+test('it should keep packages when the "keep" option is used', function (t) {
+  var input = JSON.stringify({
+    foo: 'bar',
+    baz: 'bat',
+    bat: 'man',
+    dependencies: {
+      somedep: '1.2.3',
+      somedep2: '2.3.4',
+      somedep3: '3.4.5',
+      somedep4: '6.7.8'
+    }
+  })
+
+  var expectedOutput = {
+    foo: 'bar',
+    baz: 'bat',
+    bat: 'man',
+    dependencies: {
+      somedep2: '2.3.4',
+      somedep3: '3.4.5'
+    }
+  }
+
+  var executable = path.join(__dirname, 'index.js')
+  var terminal = childProcess.spawn(executable, ['--keep', 'somedep2', '--keep', 'somedep3'])
+
+  var output = ''
+  terminal.stdout.on('data', function (data) {
+    output += data
+  })
+
+  terminal.on('exit', function (code) {
+    var parsedOutput = JSON.parse(output)
+    t.equals(code, 0)
+    t.same(parsedOutput, expectedOutput)
+    t.end()
+  })
+
+  terminal.stdin.write(input)
+  terminal.stdin.end()
+})
